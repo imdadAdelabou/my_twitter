8985fffe13e732c717dcba001c9a6d45f29ab323
imdad Adelabou <imdadadelabou0@gmail.com>
2021-10-02 15:36:21 +0100

    just put error conditions on the content that the user enters before storing in the database recover the size of a string in ruby

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,18 @@ class PostsController < ApplicationController
   def index
   end
   def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "Successfully added"
+    else
+      render :new
+    end
+  end
+  private
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
