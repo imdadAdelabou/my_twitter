b099b54bafd1fbdf9ec283e91e78c44c5479ecc0
imdad Adelabou <imdadadelabou0@gmail.com>
2021-10-03 16:20:18 +0100

    appliquer les modifications du menthor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,11 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
     if params[:back]
@@ -18,6 +20,7 @@ class PostsController < ApplicationController
       end
     end
   end
+
   def update
     if @post.update(post_params)
       redirect_to posts_path, notice: "Modification successfully completed"
@@ -25,24 +28,30 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+
   def confirm
     @post = Post.new(post_params)
     render :new if @post.invalid?
   end
+
   def destroy
     @post.destroy
     redirect_to posts_path, notice: "Post successfully deleted"
   end
+
   def edit
     @post = Post.find(params[:id])
   end
+
   def show
     @post = Post.find(params[:id])
   end
+  
   private
   def post_params
     params.require(:post).permit(:content)
   end
+
   def set_post
     @post = Post.find(params[:id])
   end
